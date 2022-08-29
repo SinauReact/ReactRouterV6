769e8e26e7ffad696b9eddb1f96efd4290a29792
argianardi <argianardi1435@gmail.com>
2022-08-29 21:35:43 +0700

    add useNavigate

---
+++ src/pages/Detail.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Detail = () => {
+  return (
+    <div>
+      <p>Detail</p>
+    </div>
+  );
+};
+
+export default Detail;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Home = () => {
+  const [users, setUsers] = useState([]);
+  const navigate = useNavigate();
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((resp) => resp.json())
+      .then((data) => {
+        setUsers(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const handleGotoDetail = (id) => {
+    navigate(`/${id}`);
+  };
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <ul>
+        {users.map((user) => {
+          return (
+            <li key={user.id}>
+              {user.name}
+              <button onClick={() => handleGotoDetail(user.id)}>
+                Go to Detail
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
