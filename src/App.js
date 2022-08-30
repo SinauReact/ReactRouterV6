e91e90a4c72e421575ac6e6b46a17fee1c1ac8ea
argianardi <argianardi1435@gmail.com>
2022-08-30 23:18:57 +0700

    add nested route

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Detail from "./pages/Detail";
+import Post from "./components/Post";
+import Product from "./components/Product";
 
 function App() {
   return (
     <BrowserRouter>
       <nav>
-        <Link to={"/"}> Home </Link> | <Link to={"login"}>Login</Link>
+        <Link to="/"> Home </Link> | <Link to="login">Login</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path=":id" element={<Detail />} />
+        <Route path=":id" element={<Detail />}>
+          <Route path={"post"} element={<Post />} />
+          <Route path={"product"} element={<Product />} />
+        </Route>
         <Route path="login" element={<Login />} />
       </Routes>
     </BrowserRouter>
