c3b68e7c0a12c5a1b3584797144172af19395ce1
argianardi <argianardi1435@gmail.com>
2022-09-05 23:30:09 +0700

    add navigate

--- src/App.js
+++ src/App.js
@@ -1,33 +1,19 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import "./App.css";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Detail from "./pages/Detail";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Detail from "./pages/Detail";
-import Post from "./components/Post";
-import Product from "./components/Product";
-import ProtectedRoute from "./routes/ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <nav>
-        <Link to="/"> Home </Link> | <Link to="login">Login</Link>
+        <Link to={"/"}>Home</Link> | <Link to={"/login"}>Login</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route
-          path=":id"
-          element={
-            <ProtectedRoute>
-              {" "}
-              <Detail />
-            </ProtectedRoute>
-          }
-        >
-          <Route path={"post"} element={<Post />} />
-          <Route path={"product"} element={<Product />} />
-        </Route>
-        <Route path="login" element={<Login />} />
+        <Route path="/:id" element={<Detail />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,38 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  const handleDetailPage = (id) => {
+    navigate(`/${id}`);
+  };
+
   return (
-    <div>
-      <p>Home page</p>
-    </div>
+    <>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name}
+            <button onClick={() => handleDetailPage(user.id)}>
+              Go To Details
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
