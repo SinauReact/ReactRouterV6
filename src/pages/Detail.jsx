73d8609d440d3b57450ed3e3a66ff325df7d8382
argianardi <argianardi1435@gmail.com>
2022-09-06 01:01:06 +0700

    add params

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
 
 const Detail = () => {
+  const { id } = useParams();
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then((response) => response.json())
+      .then((data) => setUser(data))
+      .catch((error) => console.log(error));
+  }, [id]);
+
   return (
-    <div>
-      <p>Detail page</p>
-    </div>
+    <>
+      <p>Detail Page</p>
+      <p>params id: {id}</p>
+      <pre> {JSON.stringify(user, null, 2)} </pre>
+    </>
   );
 };
 
