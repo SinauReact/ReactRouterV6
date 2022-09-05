b616f85baeb0edf7a6bfba828760b3605a712462
argianardi <argianardi1435@gmail.com>
2022-09-05 22:05:04 +0700

    prepare Detail page

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,29 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { useParams, Outlet, Link } from "react-router-dom";
+import React from "react";
 
 const Detail = () => {
-  const { id } = useParams();
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-      .then((resp) => resp.json())
-      .then((data) => {
-        setUser(data);
-      })
-      .catch((err) => console.log(err));
-  }, [id]);
-
   return (
     <div>
-      <p>Detail Page</p>
-      <p>Params id: {id}</p>
-      <pre> {JSON.stringify(user, null, 2)} </pre>
-      <hr />
-      <nav>
-        <Link to="post"> Post</Link> | <Link to="product">Product</Link>
-      </nav>
-      <Outlet />
+      <p>Detail page</p>
     </div>
   );
 };
