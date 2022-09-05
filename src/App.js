21d04efa7670863ecba1b2633ae99026f0933f26
argianardi <argianardi1435@gmail.com>
2022-09-06 01:28:10 +0700

    add configurate child route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Post from "./components/Post";
+import Product from "./components/Product";
 import Detail from "./pages/Detail";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -12,7 +14,10 @@ function App() {
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/:id" element={<Detail />} />
+        <Route path="/:id" element={<Detail />}>
+          <Route path={"post"} element={<Post />} />
+          <Route path={"product"} element={<Product />} />
+        </Route>
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
